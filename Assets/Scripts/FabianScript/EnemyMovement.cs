1b2873a81f40d9ad72ea17f803951287b94273c1
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2020-09-10 12:18:00 +0200

    Började arbeta på enemy movement och har skapat script sprite och ground.

---
+++ Assets/Scripts/FabianScript/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Rigidbody2D rbody2d; //Component rigidbody
+    public float enemySpeed; //Variabel for enemy speed
+    
+    void Start()
+    {
+        rbody2d = gameObject.GetComponent<Rigidbody2D>(); //Gets rigidbody component
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
