fd4d4b77e1e5c1576c7feab5924c0e2132d264d4
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2020-09-17 12:21:52 +0200

    Fixat med enemy. Den kan rör sig. Kan rotera.

--- Assets/Scripts/FabianScript/EnemyMovement.cs
+++ Assets/Scripts/FabianScript/EnemyMovement.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    public Rigidbody2D rbody2d; //Component rigidbody
+    public Rigidbody2D rBody; //Component rigidbody
     public float enemySpeed; //Variabel for enemy speed
+    public bool enemyGrounded;
     
     void Start()
     {
-        rbody2d = gameObject.GetComponent<Rigidbody2D>(); //Gets rigidbody component
+        rBody = gameObject.GetComponent<Rigidbody2D>(); //Gets rigidbody component
     }
 
     
     void Update()
     {
+        rBody.velocity = new Vector2(enemySpeed, 0.0f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.Rotate(0.0f, -180f, 0.0f);
         
     }
 }
