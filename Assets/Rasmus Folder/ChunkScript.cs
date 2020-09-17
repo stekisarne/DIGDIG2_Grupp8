5c522fed2d9c6a69fea6ee6e674cda92a6776750
Rasmus Holmström <rasmus.holmstrom@elev.ga.lbs.se>
2020-09-17 14:05:08 +0200

    +re added chunkscript text +added more chunks +fixed non existent variable

--- Assets/Rasmus Folder/ChunkScript.cs
+++ Assets/Rasmus Folder/ChunkScript.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class ChunkScript : MonoBehaviour
 {
+    ChunkManagerScript chunkManagerScript;
+    private void Awake()
+    {
+        chunkManagerScript = GameObject.FindGameObjectWithTag("ChunkHandler").transform.GetComponent<ChunkManagerScript>();
+    }
 
-
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            chunkManagerScript.LoadChunk();
+        }
+    }
 }
