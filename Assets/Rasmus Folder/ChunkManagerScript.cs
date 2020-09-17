e974dcdb4174fdacf2486caec59d94bea2973872
Rasmus Holmström <rasmus.holmstrom@elev.ga.lbs.se>
2020-09-17 11:46:00 +0200

    -Changed name of ChunkScript to ChunkManagerScript +added ChunkScript +Changed chunk colliders

---
+++ Assets/Rasmus Folder/ChunkManagerScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkManagerScript : MonoBehaviour
+{
+    [SerializeField] GameObject[] chunks;
+    [SerializeField] GameObject chunkParent;
+    [SerializeField] GameObject currentChunk;
+    public GameObject LoadedChunk;
+    GameObject chunkToLoad;
+    GameObject previousChunk;
+    int distanceFromStart = 0;
+    public ChunkScript chunkScript;
+    public GameObject player;
+    float distancePlayerChunk;
+    
+    void Start()
+    {
+
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadChunk();
+            DeloadChunk();
+        }
+        
+    }
+
+    void LoadChunk()
+    {
+        int i;
+
+        Vector3 newPos;
+
+        distanceFromStart = distanceFromStart - 20;
+
+        i = Random.Range(0, chunks.Length);
+
+        newPos = new Vector3(0f, (float)distanceFromStart, 0f);
+
+        chunkToLoad = chunks[i];
+
+        LoadedChunk = Instantiate(chunkToLoad, transform.position + newPos, Quaternion.identity, chunkParent.transform);
+
+        chunkScript = LoadedChunk.GetComponent<ChunkScript>();
+
+        print("loaded " + chunkToLoad.name);
+    }
+
+    void DeloadChunk()
+    {
+        previousChunk = currentChunk;
+        currentChunk = LoadedChunk;
+        print("Deloaded " + previousChunk);
+        Destroy(previousChunk);
+
+    }
+}
--- Assets/Rasmus Folder/ChunkScript.cs
+++ Assets/Rasmus Folder/ChunkScript.cs
@@ -4,54 +4,14 @@ using UnityEngine;
 
 public class ChunkScript : MonoBehaviour
 {
-    [SerializeField] GameObject[] chunks;
-    [SerializeField] GameObject chunkParent;
-    [SerializeField] GameObject currentChunk;
-    public GameObject LoadedChunk;
-    GameObject chunkToLoad;
-    GameObject previousChunk;
-    int distanceFromStart = 0;
-    
-    void Start()
+    public bool inTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        inTrigger = true;
     }
-
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            LoadChunk();
-            DeloadChunk();
-        }
-
-    }
-
-    void LoadChunk()
-    {
-        int i;
-
-        Vector3 newPos;
-
-        distanceFromStart = distanceFromStart - 20;
-
-        i = Random.Range(0, chunks.Length);
-
-        newPos = new Vector3(0f, (float)distanceFromStart, 0f);
-
-        chunkToLoad = chunks[i];
-
-        LoadedChunk = Instantiate(chunkToLoad, transform.position + newPos, Quaternion.identity, chunkParent.transform);
-
-        print("loaded " + chunkToLoad.name);
-    }
-
-    void DeloadChunk()
-    {
-        previousChunk = currentChunk;
-        currentChunk = LoadedChunk;
-        print("Deloaded " + previousChunk);
-        Destroy(previousChunk);
-
+        inTrigger = false;
     }
 }
