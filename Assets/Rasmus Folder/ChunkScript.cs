2b5250ea40433f615f36ac3abe9c482f2fc4e2f3
Rasmus Holmström <rasmus.holmstrom@elev.ga.lbs.se>
2020-09-17 10:35:22 +0200

    +basic chunk generation

--- Assets/Rasmus Folder/ChunkScript.cs
+++ Assets/Rasmus Folder/ChunkScript.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class ChunkScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] chunks;
+    [SerializeField] GameObject chunkParent;
+    GameObject currentChunk;
+    GameObject chunkToLoad;
+    GameObject previousChunk;
+    public GameObject LoadedChunk;
+    int distanceFromStart = 0;
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadChunk();
+        }
+    }
+
+    void LoadChunk()
+    {
+        int i;
+
+        Vector3 newPos;
+
+        distanceFromStart = distanceFromStart - 20;
+
+        i = Random.Range(0, chunks.Length);
+
+        newPos = new Vector3(0f, (float)distanceFromStart, 0f);
+
+        chunkToLoad = chunks[i];
+
+        LoadedChunk = Instantiate(chunkToLoad, transform.position + newPos, Quaternion.identity, chunkParent.transform);
+
+        print("loaded " + chunkToLoad.name);
     }
 }
