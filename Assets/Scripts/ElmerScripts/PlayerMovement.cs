cff4642c31466d37330a57db70d929ad7c5b297b
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2020-09-17 12:19:40 +0200

    added movement and sprites for hero

---
+++ Assets/Scripts/ElmerScripts/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rBody;
+
+    public enum PlayerState { Moving, Fighting, Idling }
+    PlayerState currentState;
+    // currentstate = PlayerState.Idling;
+
+    [Header("restrictions")]
+    public float maxMoveSpeed;
+    public float maxFallSpeed;
+
+    [Header("stats")]
+    public float moveSpeed;
+    public float jumpForce;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+        Jump();
+
+
+
+    }
+
+    public void Walk()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            rBody.velocity = new Vector2 (Input.GetAxis("Horizontal") * moveSpeed, rBody.velocity.y);
+            if (Input.GetAxis("Horizontal") >= 0)
+            {
+                transform.rotation = new Quaternion (0, 0, 0, 0);
+            }
+            if (Input.GetAxis("Horizontal") <= 0)
+            {
+                transform.rotation = new Quaternion (0, 180, 0, 0);
+            }
+        }
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("jump");
+            rBody.velocity = new Vector2(rBody.velocity.x, jumpForce);
+        }
+    }
+}
