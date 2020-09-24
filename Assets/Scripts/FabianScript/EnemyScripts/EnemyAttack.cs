0b4332731d0b1cdff1c7c76f408f03b034827acc
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2020-09-24 12:16:28 +0200

    Klar med enemy movement. På börjat enemy attack.

---
+++ Assets/Scripts/FabianScript/EnemyScripts/EnemyAttack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float enemyDamage; //Sets the enemy damage 
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            //Player health - enemyDamage.
+            //Player takes X amount of damage.
+            //Needs conection with a player health script
+        }
+    }
+}
