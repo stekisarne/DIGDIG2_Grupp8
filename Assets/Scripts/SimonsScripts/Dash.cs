33191cf537110be6e5513780c1ba1623d81098f0
Simon Schachinger <simon.schachinger@elev.ga.lbs.se>
2020-09-24 12:21:12 +0200

    Börjat på en velocity dash

--- Assets/Scripts/SimonsScripts/Dash.cs
+++ Assets/Scripts/SimonsScripts/Dash.cs
@@ -5,32 +5,58 @@ using UnityEngine;
 public class Dash : MonoBehaviour
 {
     private Camera cam;
+
+    Vector2 playerPosition;
+    Vector2 dashPoint;
+    Rigidbody2D rBody;
+    public float maxDistance;
+    public float dashSpeed;
     void Start()
     {
         cam = Camera.main;
+        rBody = GetComponent<Rigidbody2D>();
+        playerPosition = gameObject.transform.position;
     }
 
-    
+
     void Update()
     {
-        
+        DashToMouse();
     }
+    void DashToMouse()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
 
+            Vector2 desiredDirection = dashPoint; // set this to the direction you want.
+            Vector2 dashDirection = desiredDirection.normalized * rBody.velocity.magnitude;
+
+            rBody.velocity = dashDirection;
+        }
+    }
     void GetDashPoint()
     {
-        Vector3 dashPoint = new Vector3();
-        Event currentEvent = Event.current;
-        Vector2 mousePos = new Vector2();
 
-        // Get the mouse position from Event.
-        // Note that the y position from Event is inverted.
-        mousePos.x = currentEvent.mousePosition.x;
-        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+        Vector3 mousePos = Input.mousePosition;
 
-        dashPoint = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+        Vector2 dashPoint = new Vector2();
+        mousePos.z = 10;
+        
+        // compute where the mouse is in world space
+        
+        dashPoint = cam.ScreenToWorldPoint(mousePos);
 
         
+
     }
 
     
+    private void FixedUpdate()
+    {
+        
+    }
 }
+
+
+
+    
