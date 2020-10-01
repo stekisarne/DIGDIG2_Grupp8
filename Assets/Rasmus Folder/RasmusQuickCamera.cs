b7c3bacf1b994ed2f767b33f77bc76fe4064fae2
Rasmus Holmström <rasmus.holmstrom@elev.ga.lbs.se>
2020-10-01 10:42:39 +0200

    -Removed all old chunks +Added new chunks

--- Assets/Rasmus Folder/RasmusQuickCamera.cs
+++ Assets/Rasmus Folder/RasmusQuickCamera.cs
@@ -11,7 +11,7 @@ public class RasmusQuickCamera : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        this.transform.position = new Vector3(followTransform.position.x, followTransform.position.y, this.transform.position.z);
+        this.transform.position = new Vector3(this.transform.position.x, followTransform.position.y, this.transform.position.z);
 
 
     }
