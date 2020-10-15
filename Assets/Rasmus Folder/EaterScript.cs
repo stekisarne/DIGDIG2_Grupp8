56f34b46e68b9fe1ec8a66c54d2d29e0c687eba2
Rasmus Holmström <rasmus.holmstrom@elev.ga.lbs.se>
2020-10-15 12:18:43 +0200

    +pilstuff

--- Assets/Rasmus Folder/EaterScript.cs
+++ Assets/Rasmus Folder/EaterScript.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class EaterScript : MonoBehaviour
 {
-
-    void Start()
-    {
-
-    }
-
+    [SerializeField] GameObject Arrow;
+    [SerializeField] GameObject player;
+    float distanceBetweenEaterAndPlayer;
     void Update()
     {
         transform.Translate(0, -3 * Time.deltaTime, 0);
+
+        distanceBetweenEaterAndPlayer = Mathf.Clamp(Vector2.Distance(gameObject.transform.position/50, player.transform.position/50), 0, 1.5f);
+
+        Vector3 arrowSize = new Vector3(distanceBetweenEaterAndPlayer, distanceBetweenEaterAndPlayer);
+
+        print("distance between: " + distanceBetweenEaterAndPlayer);
+        print(arrowSize);
+
+        Arrow.transform.localScale = arrowSize;
     }
 
     void EatChunk(GameObject ChunkToDestroy)
     {
         Destroy(ChunkToDestroy, 5f);
     }
+
+    
 
     private void OnTriggerEnter2D(Collider2D other)
     {
