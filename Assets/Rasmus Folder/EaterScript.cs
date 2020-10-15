3d71090d5990e043f891890910d560a478fccf57
Rasmus Holmström <rasmus.holmstrom@elev.ga.lbs.se>
2020-10-15 10:41:02 +0200

    +Finished EaterScript

---
+++ Assets/Rasmus Folder/EaterScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EaterScript : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Translate(0, -3 * Time.deltaTime, 0);
+    }
+
+    void EatChunk(GameObject ChunkToDestroy)
+    {
+        Destroy(ChunkToDestroy, 5f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Chunk")
+        {
+            print("chunker");
+            EatChunk(other.gameObject);
+        }
+    }
+}
