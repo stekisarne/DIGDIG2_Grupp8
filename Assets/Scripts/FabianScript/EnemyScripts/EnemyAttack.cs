febaf5dbd56fa9d266af116c7d750ff71315f12b
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2020-10-15 10:51:22 +0200

    Började fortsatte med fiende animationer och fixade med attacking animation.

--- Assets/Scripts/FabianScript/EnemyScripts/EnemyAttack.cs
+++ Assets/Scripts/FabianScript/EnemyScripts/EnemyAttack.cs
@@ -6,18 +6,25 @@ public class EnemyAttack : MonoBehaviour
 {
     public int enemyDamage; //Sets the enemy damage 
     PlayerHealth playerHealth;  //A connection with the player health script
+    Animator enemyAnimator;
 
     void Start()
     {
         playerHealth = FindObjectOfType<PlayerHealth>();    //Finds the object with the PlayerHealth script. 
+        enemyAnimator = FindObjectOfType<Animator>();
     }
 
     public void OnTriggerEnter2D(Collider2D other) //Deals damage too the player character.
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
+            enemyAnimator.SetBool("NearPlayer", true);
             playerHealth.playerHp -= enemyDamage;
             playerHealth.Death();
+        }
+        else
+        {
+            enemyAnimator.SetBool("NearPlayer", false);
         }
     }
 }
