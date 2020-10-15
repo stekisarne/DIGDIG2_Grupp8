e4e35f8e4011a25488f680e3385132a650d7cdb6
Fabian Andersson <fabian.andersson2@elev.ga.lbs.se>
2020-10-15 12:17:43 +0200

    Klar med enemy attack animation men det är en bug med att en pixel artifact är kvar i animationen när den spelar.

--- Assets/Scripts/FabianScript/EnemyScripts/EnemyAttack.cs
+++ Assets/Scripts/FabianScript/EnemyScripts/EnemyAttack.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,23 @@ public class EnemyAttack : MonoBehaviour
 {
     public int enemyDamage; //Sets the enemy damage 
     PlayerHealth playerHealth;  //A connection with the player health script
+    EnemyMovement enemyMovement;
     Animator enemyAnimator;
 
     void Start()
     {
         playerHealth = FindObjectOfType<PlayerHealth>();    //Finds the object with the PlayerHealth script. 
         enemyAnimator = FindObjectOfType<Animator>();
+        enemyMovement = FindObjectOfType<EnemyMovement>();
+    }
+
+    private void Update()
+    {
+        if (playerHealth != null)
+        {
+            enemyAnimator.SetBool("PlayerIsDead", true);
+            playerHealth.Death();
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other) //Deals damage too the player character.
@@ -19,12 +31,17 @@ public class EnemyAttack : MonoBehaviour
         if (other.tag == "Player")
         {
             enemyAnimator.SetBool("NearPlayer", true);
-            playerHealth.playerHp -= enemyDamage;
-            playerHealth.Death();
+            enemyMovement.enemySpeed = 0;
         }
         else
         {
             enemyAnimator.SetBool("NearPlayer", false);
+            enemyMovement.enemySpeed = 1.5f;
         }
+    }
+
+    public void BiteAttack() //Deals damage to the player when an animationen event triggers this function
+    {
+        playerHealth.playerHp -= enemyDamage;
     }
 }
--- Assets/Scripts/FabianScript/PlayerCharacter/PlayerHealth.cs
+++ Assets/Scripts/FabianScript/PlayerCharacter/PlayerHealth.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     public int playerHp;    //A variabel for the player health points.
+    EnemyMovement enemyMovement;
+    Animator enemyAnimator;
 
-    public void Awake()
+    public void Awake() //Sets the players health to a set value
     {
         playerHp = 100;
+    }
+
+    void Start()
+    {
+        enemyAnimator = FindObjectOfType<Animator>();
+        enemyMovement = FindObjectOfType<EnemyMovement>();
     }
 
     public void Death()
     {
         if(playerHp <= 0)   //I the player characters health goes under or is equal too 0 it destroyes the game object.
         {
+            enemyAnimator.SetBool("NearPlayer", false);
+            enemyAnimator.SetBool("PlayerIsDead", true);
+            enemyMovement.enemySpeed = 1.5f;
             Destroy(this.gameObject);
         }
     }
