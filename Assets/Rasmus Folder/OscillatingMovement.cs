4ec3869c7e90ff7e20abea9ae6be2bcfac8898e3
Rasmus Holmström <rasmus.holmstrom@elev.ga.lbs.se>
2020-10-22 12:09:02 +0200

    +Oscillating movement script +Stat of Main Menu

---
+++ Assets/Rasmus Folder/OscillatingMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OscillatingMovement : MonoBehaviour
+{
+    Vector3 oscPos;
+    [SerializeField] float oscSpeed;
+    [SerializeField] float oscLength;
+    [SerializeField] float timeOffset;
+    void Start()
+    {
+        oscPos = gameObject.transform.position;
+    }
+
+    void Update()
+    {
+        while(Time.time > timeOffset)
+        {
+            oscPos.y = Mathf.PingPong(Time.time * oscSpeed, oscLength) - oscLength / 2;
+            transform.position = oscPos;
+        }
+    }
+}
