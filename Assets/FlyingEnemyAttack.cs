25a0debec040be165243f4caed042976798dc23d
Simon Schachinger <simon.schachinger@elev.ga.lbs.se>
2020-11-26 12:16:05 +0100

    Flygande fiende färdig för tillfället. har inte lyckats fixa en delay på första attacken.

--- Assets/FlyingEnemyAttack.cs
+++ Assets/FlyingEnemyAttack.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class FlyingEnemyAttack : MonoBehaviour
 {
     public int flyingEnemyDamage;
+    public float cooldown;
+    public float cooldownTimer;
     FlyingEnemyMovement flyingEnemyMovement;
     PlayerHealth playerHealth;
     void Start()
@@ -16,6 +18,25 @@ public class FlyingEnemyAttack : MonoBehaviour
     
     void Update()
     {
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    public void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            
+            
+
+            if (cooldownTimer <= 0)
+            {
+                playerHealth.playerHp -= flyingEnemyDamage;
+                cooldownTimer = cooldown;
+            }
+        }
         
     }
 }
