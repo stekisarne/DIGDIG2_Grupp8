7eb7977ff7ab6fae1211ab47907e8e25ba7d010e
LEARNET\elmer.rastamwase <elmer.rstamwase@elev.ga.lbs.se>
2021-03-04 10:56:54 +0100

    spelar mechanics klar

--- Assets/Scripts/ElmerScripts/PlayerMovement.cs
+++ Assets/Scripts/ElmerScripts/PlayerMovement.cs
@@ -18,6 +18,7 @@ public class PlayerMovement : MonoBehaviour
     public bool isFacingRight = true;
     public bool isGrounded;
     public float currentWalkCooldown;
+    public bool canTurn = true;
 
     [Header("stats")]
     public float moveSpeed;
@@ -63,7 +64,9 @@ public class PlayerMovement : MonoBehaviour
         }
         GroundWallCheck();
         FallCheck();
-        PlayerLookDir();
+        if (canTurn == true){
+            PlayerLookDir();
+        }
     }
 
    
@@ -121,7 +124,6 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-
     void WalkCooldown()
     {
         currentWalkCooldown = Mathf.Max(currentWalkCooldown - Time.deltaTime, 0);
@@ -178,6 +180,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void PlayerLookDir()
     {
+        Debug.Log(canTurn);
         if (rBody.velocity.x >= 0.3f)
         {
             isFacingRight = true;
